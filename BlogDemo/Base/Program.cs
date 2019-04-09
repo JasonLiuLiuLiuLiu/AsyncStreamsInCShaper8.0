0ec0286dded857d57e1a078cf0959953bf86c8d2
Zhenyu,Liu <liuzhenyulive@live.com>
2019-04-09 21:26:05 +0800

    Add common

---
+++ BlogDemo/Base/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Base
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
---
+++ BlogDemo/Common/ConsoleExt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace Common
+{
+    public class ConsoleExt
+    {
+        public static void WriteLine(string message)
+        {
+            Console.WriteLine(GetCurrentTimeAndThreadId() + message);
+        }
+
+        private static string GetCurrentTimeAndThreadId()
+        {
+            return $"(Time:{DateTime.Now},Thread:{Thread.CurrentThread.ManagedThreadId}):";
+        }
+    }
+}
